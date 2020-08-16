bbdab33ad09a6da07fbd6e974e94631db1dd0565
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-08-15 21:40:32 -0500

    [ADD] initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+  "Daniel",
+  "Laura",
+  "Maria",
+  "Camilo",
+  "Diego"
+]
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(message)
+}
+
+module.exports = { randomMsg };
